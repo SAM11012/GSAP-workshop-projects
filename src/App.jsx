a48ae24d2104cdc25c52c4f5fb6047b9afe1ce76
Samyak Meshram <samyakmeshram008@Samyaks-MacBook-Pro-2.local>
2025-06-27 16:01:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import gsap from "gsap";
+import {ScrollTrigger,SplitText} from "gsap/all";
+gsap.registerPlugin(ScrollTrigger,SplitText);
+const App = () => {
+    return (
+        <div className='text-3xl text-red-300 flex-center h-[100vh] '>App</div>
+    )
+}
+export default App
